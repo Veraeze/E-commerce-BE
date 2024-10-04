c227a7397c75a26ad3e0a8850c009c47949487ff
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2024-10-04 15:00:49 +0100

    register user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,11 @@
+import userModel from "../models/userModel.js"
+import validator from "validator"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
+const createToken = (id) => {
+        return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 // route for user login
 const loginUser = async (req, res) => {
@@ -7,7 +14,47 @@ const loginUser = async (req, res) => {
 
 // route for user registration
 const registerUser = async (req, res) => {
-        res.json({msg: "Register API Working"})
+
+        try{
+              const {name, email, password} = req.body;
+ 
+               // check user already exists or not
+                const exists = await userModel.findOne({email});
+                if (exists) {
+                        return res.json({success:false, message:"User already exists"})
+                }
+
+                //validate email format and strong password
+                if (!validator.isEmail(email)) {
+                        return res.json({success:false, message:"Please enter a valid email"})
+
+                }
+                if (password.length < 8) {
+                        return res.json({success:false, message:"Password minimum length is 8"})
+
+                }
+
+                // hashing user password
+                const salt = await bcrypt.genSalt(10)
+                const hashedPassword = await bcrypt.hash(password, salt)
+
+                const newUser = new userModel({
+                        name,
+                        email,
+                        password:hashedPassword
+                })
+
+                const user = await newUser.save()
+                const token = createToken(user._id)
+
+                res.json({success:true,token})
+
+        }catch (error){
+                console.log(error);
+                res.json({success:false, message:error.message})
+                
+        }
+        
 }
 
 // route for admin login
