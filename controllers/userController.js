f324338167b7b04952394fd03aa418586abbb5ff
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2024-10-04 14:27:30 +0100

    backend setup

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+// route for user login
+const loginUser = async (req, res) => {
+
+}
+
+// route for user registration
+const registerUser = async (req, res) => {
+        res.json({msg: "Register API Working"})
+}
+
+// route for admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export {loginUser, registerUser, adminLogin}
